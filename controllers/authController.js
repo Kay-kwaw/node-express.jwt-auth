d486893923b755d23cc085a5fb280230d0cafd11
kwaw-kumi <kwawkumi9@gmail.com>
2023-02-13 20:08:53 -0800

    validation failed

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,15 +3,15 @@ const User = require('../models/User');
 const handleErrors = (err)=>{
     console.log(err.message, err.code);
     //This is the object that will be sent to the user as json 
-    let errors = {email: '', password: '' };
+    // let errors = {email: '', password: '' };
 
-    //Validation errors
-    if (err.message.includes('user validation failed')) {
-        Object.values(err.errors).forEach(
-            ({properties}) => {
-                errors[properties.path] = properties.message;
-             });
-    }
+    // //Validation errors
+    // if (err.message.includes('user validation failed')) {
+    //     Object.values(err.errors).forEach(
+    //         ({properties}) => {
+    //             errors[properties.path] = properties.message;
+    //          });
+    // }
 }
 
 
@@ -20,15 +20,16 @@ module.exports.signup_get = (req, res, next) =>{
 };
 
 module.exports.signup_post = async(req, res) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     try{
-    const user= await User.create({email,password});
-    res.status(200).json(user);
+    const user= await User.create({ email,password });
+    res.status(400).json(user);
     }
     catch(err){
-       const errors = handleErrors(err);
-        res.status(400).json({ errors });
+    //    const errors =
+        handleErrors(err);
+        res.status(201).json({ errors });
     };
 };
 
--- models/User.js
+++ models/User.js
@@ -3,16 +3,16 @@ const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
     email: {
-        type: 'string',
-        required:[true, "Please enter an email address"] ,
+        type: String,
+        required:[true, 'Please enter an email address'] ,
         unique: true,
         lowercase: true,
-        validate: [isEmail, "Please enter a valid email address"]
+        validate: [isEmail, 'Please enter a valid email address']
     },
     password: {
-        type: 'string',
-        required: [true, "Please enter a valid password"],
-        minlength: [6, "Minimum password length is 6 characters"],
+        type: String,
+        required: [true, 'Please enter a valid password'],
+        minlength: [6, 'Minimum password length is 6 characters'],
     },
 
 });
