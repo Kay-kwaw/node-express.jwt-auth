f7d432cf40c609bcf95870bd7f397504255f5edd
kwaw-kumi <kwawkumi9@gmail.com>
2023-02-14 05:55:24 -0800

    cookies

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoute =  require('./routes/authRoute');
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 
@@ -8,6 +10,7 @@ const app = express();
 // middleware for the styling
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());   
 
 // view engine
 app.set('view engine', 'ejs');
@@ -22,3 +25,25 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoute);
+
+//cookies
+//These are used to store user's data in the browser
+// and also used to track internet activities
+
+app.get('/set-cookies', (req, res) => {
+  res.cookie('newUser', false);
+  res.cookie('isEmployer', true, {
+    maxAge: 1000*60*60*24, httpOnly: true
+  });
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  res.send('you set cookies')
+  
+});
+
+app.get('read-cookies', (req, res) => {
+ 
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies)
+});
+
